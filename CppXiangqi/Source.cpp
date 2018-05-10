9394c97efd0ce1342400fc117888f4e7c330c65e
Cheng Yuan <cy1818cy@bupt.edu.cn>
2018-05-11 07:29:35 +0800

    v1.0.0001

--- CppXiangqi/Source.cpp
+++ CppXiangqi/Source.cpp
@@ -12,9 +12,19 @@ using namespace std;
 
 const int __CHESSNUM[]={0,2,2,2,2,2,5,1};
 const char __CHESSNAM[2][8][3]={"暗","車","馬","炮","仕","象","卒","将",
-								"　","车","马","炮","士","相","兵","帅",};
+								"　","车","马","炮","士","相","兵","帅",
+								};
+
+inline int __COUNT_B1(int t){
+	// 计算t中二进制下1的数量。
+	register int r=0;
+	for(; t; t&=t-1)r++;
+	return r;
+}
 
 struct Node;
+struct Mana;
+struct State;
 
 struct Mana{
 	char px, py;												// 位置
@@ -43,11 +53,15 @@ struct State{
 	char hid[2][8];												// 每方剩余暗子数量
 	char tothid[2];												// 每方暗子总量
 	int score;													// 分数
-	//char lastmove[2];											// 最近移动的子
 
 	// 获取(x,y)位置上的棋子
 	Mana& get(char x, char y){
 		return ~bd[x][y]?ch[0][bd[x][y]]:noMana; 
+	}
+
+	// 检查(x,y)位置是否有棋子，等价于get(x,y).shape()，效率较高
+	bool has(char x, char y){
+		return ~bd[x][y];
 	}
 
 	// 将棋盘变成开局时的模样
@@ -90,18 +104,19 @@ struct State{
 			bd[ch[0][i].px][ch[0][i].py]=i;
 		for(int i=0; i<32; i++)
 			calcMove(ch[0][i]);									// 计算移动
+		for(int i=0; i<2; i++){
+			hid[i][1]=hid[i][2]=hid[i][3]=hid[i][4]=hid[i][5]=2;
+			hid[i][6]=5;
+			tothid[i]=15;
+		}
 	}
 
 	// 移动一颗子，如果是暗子第一次移动，在newchess中指定变成的明子
-	void move(char sx, char sy, char ex, char ey, char newchess=0){
+	void move(char sx, char sy, char ex, char ey){
 		get(ex, ey).eated();									// 被吃的子
-		if(newchess){											// 暗子被翻开
-			get(sx, sy).kind=newchess|8;
-			hid[get(sx,sy).isred()][get(sx, sy).shape()]--;
-			tothid[get(sx, sy).isred()]--;
-		}
+		get(sx, sy).px=ex;
+		get(sx, sy).py=ey;										// 修改棋子坐标
 
-		lastmove[get(ex, ey).isred()]=bd[sx][sy];				// 最近移动的子
 		bd[ex][ey]=bd[sx][sy];									// 改变棋盘指针
 		bd[sx][sy]=-1;
 
@@ -109,8 +124,13 @@ struct State{
 			if(!ch[side][i].eaten())
 				updateMove(sx, sy, ex, ey, ch[side][i]);
 
-
 		// 更新场面相关信息
+	}
+
+	void flushOut(char sx, char sy, char newchess){
+		get(sx, sy).kind=newchess|8;
+		hid[get(sx, sy).isred()][get(sx, sy).shape()]--;
+		tothid[get(sx, sy).isred()]--;
 	}
 
 	// 更新当前棋子的合法移动
@@ -125,22 +145,22 @@ struct State{
 		switch(ch.shape()){
 		case 1:													// 车
 		{
-			for(char x=ch.x()-1; x>=0; x--)if(get(x,ch.y()).shape()){
+			for(char x=ch.x()-1; x>=0; x--)if(has(x,ch.y())){
 				if(get(x,ch.y()).isred()^side)ch.addmove(x);
 				break;
 			}
 			else ch.addmove(x);
-			for(char x=ch.x()+1; x<=8; x++)if(get(x,ch.y()).shape()){
+			for(char x=ch.x()+1; x<=8; x++)if(has(x,ch.y())){
 				if(get(x,ch.y()).isred()^side)ch.addmove(x);
 				break;
 			}
 			else ch.addmove(x);
-			for(char y=ch.y()-1; y>=0; y--)if(get(ch.x(),y).shape()){
+			for(char y=ch.y()-1; y>=0; y--)if(has(ch.x(),y)){
 				if(get(ch.x(),y).isred()^side)ch.addmove(y+16);
 				break;
 			}
 			else ch.addmove(y+16);
-			for(char y=ch.y()+1; y<=9; y++)if(get(ch.x(),y).shape()){
+			for(char y=ch.y()+1; y<=9; y++)if(has(ch.x(),y)){
 				if(get(ch.x(),y).isred()^side)ch.addmove(y+16);
 				break;
 			}
@@ -148,7 +168,39 @@ struct State{
 		}break;
 		case 2:													// 马
 		{
-
+			char x=ch.x(), y=ch.y();
+			if(x<=6 && !has(x+1, y)){
+				if(y!=9 && (!has(x+2, y+1) || (get(x+2, y+1).isred()^side))){
+					ch.addmove(1);
+				}
+				if(y!=0 && (!has(x+2, y-1) || (get(x+2, y-1).isred()^side))){
+					ch.addmove(7);
+				}
+			}
+			if(y>=2 && !has(x, y-1)){
+				if(x!=8 && (!has(x+1, y-2) || (get(x+1, y-2).isred()^side))){
+					ch.addmove(6);
+				}
+				if(x!=0 && (!has(x-1, y-2) || (get(x-1, y-2).isred()^side))){
+					ch.addmove(4);
+				}
+			}
+			if(x>=2 && !has(x-1, y)){
+				if(y!=0 && (!has(x-2, y-1) || (get(x-2, y-1).isred()^side))){
+					ch.addmove(5);
+				}
+				if(y!=9 && (!has(x-2, y+1) || (get(x-2, y+1).isred()^side))){
+					ch.addmove(3);
+				}
+			}
+			if(y<=6 && !has(x, y+1)){
+				if(x!=0 && (!has(x-1, y+2) || (get(x-1, y+2).isred()^side))){
+					ch.addmove(2);
+				}
+				if(x!=8 && (!has(x+1, y+2) || (get(x+1, y+2).isred()^side))){
+					ch.addmove(0);
+				}
+			}
 		}break;
 		case 3:													// 炮
 		{
@@ -173,8 +225,29 @@ struct State{
 		}
 	}
 
-	int getScore(){
-		return rand()%10000;
+	// 计算当前场面的评价得分
+	int getScore(bool side){
+		// 只算一边
+		const static int sc[]={0,100,60,60,50,30,20,50000};
+		const static int pw[]={0,6,4,5,4,3,4,2};
+		int ret=0;
+		
+		int mid=0;
+		for(int i=1; i<8; i++)mid+=sc[i]*hid[side][i];
+		mid/=tothid[side];
+		for(int i=0; i<16; i++){
+			Mana m=ch[side][i];
+			if(m.eaten()){
+				if(m.shown())
+					ret-=sc[m.shape()];	// 如果被吃掉的子，就减去该子的子力
+				else ret-=mid;			// 如果吃的是暗子，就减去平均子力
+			}
+			else{
+				ret+=__COUNT_B1(m.moves)*pw[m.shape()];
+										// 否则计算该子对棋盘的控制能力。
+			}
+		}
+		return ret;
 	}
 
 	void doIt(char sx, char sy, char ex, char ey, vector<Node*>& v);
@@ -187,16 +260,40 @@ struct State{
 		for(char t=1;; t++)if((temp-=hid[side][t])<0)
 			return t;											
 	}
-}_;
+};
 
 struct Node{
-	State s;
-	char sx, sy, ex, ey;
-	vector<Node*> v;
-	bool operator< (const Node& b) const{
+	// Node 表示搜索时的一个节点，构成一棵搜索树。
+	State s;								// 该节点对应的场面
+	char sx, sy, ex, ey;					// 记录此节点的上一步。我觉得记录在state中更好。
+	vector<Node*> v;						// 孩子们
+	Node* to;								// 大儿子
+	bool operator< (const Node& b) const{	// 用于sort
 		return s.score<b.s.score;
 	}
-};
+}__;
+
+State& _=__.s;
+
+void showState(State s){
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0xF0);
+	for(int j=9; j>=0; j--){
+		for(int i=0; i<=8; i++){
+			if(s.get(i, j).isred())
+				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0xFC);
+			if(!s.has(i, j))
+				printf(__CHESSNAM[0][8]);
+			else if(!s.get(i, j).shown())
+				printf(__CHESSNAM[0][0]);
+			else
+				printf(__CHESSNAM[s.get(i, j).isred()][s.get(i, j).shape()]);
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0xF0);
+			if(i<8)putchar(' ');
+		}
+		puts("");
+	}
+	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+}
 
 void State::doIt(char sx, char sy, char ex, char ey, vector<Node*>& v){
 	Node* temp = new Node{*this,sx,sy,ex,ey};
@@ -212,57 +309,66 @@ void State::perfMove(int side, vector<Node*>& v){
 			for(char x=0; x<9; x++)if(m.moves&(1<<x))doIt(m.x(), m.y(), x, m.y(), v);
 			for(char y=0; y<10; y++)if(m.moves&(1<<y+16))doIt(m.x(), m.y(), m.x(), y, v);
 		}break;
+		case 2:{
+			if(m.moves&1)doIt(m.x(), m.y(), m.x()+1, m.y()+2, v);
+			if(m.moves&2)doIt(m.x(), m.y(), m.x()+2, m.y()+1, v);
+			if(m.moves&4)doIt(m.x(), m.y(), m.x()-1, m.y()+2, v);
+			if(m.moves&8)doIt(m.x(), m.y(), m.x()-2, m.y()+1, v);
+			if(m.moves&16)doIt(m.x(), m.y(), m.x()-1, m.y()-2, v);
+			if(m.moves&32)doIt(m.x(), m.y(), m.x()-2, m.y()-1, v);
+			if(m.moves&64)doIt(m.x(), m.y(), m.x()+1, m.y()-2, v);
+			if(m.moves&128)doIt(m.x(), m.y(), m.x()+2, m.y()-1, v);
+		}break;
 		default:assert(true);
 		}
 	}
 }
 
-
-void wide_dfs(Node& root, int side, int depth){
+void wide_dfs(Node& root, bool side, int depth){
+	void showState(State s);
+	printf("Deep = %d.\n", depth);
+	showState(root.s);
 	if(depth==0){
-		root.s.score=root.s.getScore();									// 计算当前场面得分
+		root.s.score=root.s.getScore(side)-root.s.getScore(!side);	// 计算当前场面得分
+		printf("Score = %d.\n", root.s.score);
 		return;
 	}
-	vector<Node*>& Vec = root.v;
+	auto& Vec = root.v;
 	root.s.perfMove(side, Vec);
 	for(auto n:Vec){
 		if(!n->s.get(n->ex,n->ey).shown()){
 			State temp = n->s;
 			int totscore = 0;
 			for(int i=1; i<8; i++)if(n->s.hid[side][i]){
-				temp.get(n->ex,n->ey).kind = 8+i;
+				temp.flushOut(n->ex, n->ey, i);
+				temp.calcMove(temp.get(n->ex, n->ey));
 				Node* add = new Node{temp,n->sx,n->sy,n->ex,n->ey};
 				n->v.push_back(add);
 				wide_dfs(*add, !side, depth-1);
 				totscore+=add->s.score*n->s.hid[side][i];
 			}
-			root.s.score=totscore/n->s.tothid[side];
+			n->s.score=totscore/n->s.tothid[side];
 		}
 		else{
 			wide_dfs(*n, !side, depth-1);
-			root.s.score=max(root.s.score, -n->s.score);
+			if(root.s.score+n->s.score<0){
+				root.s.score=-n->s.score;
+				root.to=n;
+			}
 		}
 	}
 }
 
-void showState(State s){
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0xF0);
-	for(int j=9; j>=0; j--){
-		for(int i=0; i<=8; i++){
-			if(s.get(i, j).isred())
-				SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0xFC);
-			if(!s.get(i, j).shape())
-				printf(__CHESSNAM[0][8]);
-			else if(!s.get(i, j).shown())
-				printf(__CHESSNAM[0][0]);
-			else
-				printf(__CHESSNAM[s.get(i, j).isred()][s.get(i, j).shape()]);
-			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 0xF0);
-			if(i<8)putchar(' ');
-		}
-		puts("");
+void show_dfs(Node& root, int showd, int depth){
+	if(showd<depth){
+
 	}
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), 7);
+}
+
+void debug_dfs(Node& root,int depth=0){
+	printf("第 %d 层，当前步(%d,%d)->(%d,%d)，场面分 %d。\n", depth, root.sx, root.sy, root.ex, root.ey, root.s.score);
+	//showState(root.s);
+	for(auto n:root.v) debug_dfs(*n, depth+1);
 }
 
 int main(){
@@ -277,8 +383,19 @@ int main(){
 		}
 		else if(command=="move"){
 			int sx, sy, ex, ey, newchess;
-			cin>>sx>>sy>>ex>>ey>>newchess;
-			_.move(sx, sy, ex, ey, newchess);
+			cin>>sx>>sy>>ex>>ey;
+			_.move(sx, sy, ex, ey);
+			if(!_.get(ex, ey).shown()){
+				cin>>newchess;
+				if(!newchess)newchess=_.getRandomHidden(_.get(ex,ey).isred());
+				_.flushOut(ex, ey, newchess);
+			}
+		}
+		else if(command=="compute"){
+			cin>>command;
+			if(command=="red"){
+				wide_dfs(__, 0, 1);
+			}
 		}
 		else if(command=="debug"){
 			cin>>command;
@@ -300,7 +417,7 @@ int main(){
 				}
 			}
 			else if(command=="dfs"){
-				
+				debug_dfs(__);
 			}
 		}
 	}
